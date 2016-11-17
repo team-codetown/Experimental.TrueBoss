30ecea5bf8c77e4794217d4aa6d068550afb1801
Damian Kowalczyk <damikon8@gmail.com>
2016-11-17 21:10:04 +0100

    Added Data Objects

---
+++ DataObjects/Map/Field.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects.Map
+{
+    class Field
+    {
+        public int cordX { get; set; }
+        public int cordY { get; set; }
+        public Resource Resource { get; set; }
+    }
+}
---
+++ DataObjects/Map/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects.Map
+{
+    class Map
+    {
+        public List<Field> Fields { get; set; }
+
+        public Map()
+        {
+            Fields = new List<Field>();
+        }
+    }
+}
