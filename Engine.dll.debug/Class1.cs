1a1de5dbf289a1371a4aa3107ca4e867617a0b83
Bartosz Szczepański-Bernaś <szczp4nski@gmail.com>
2016-11-28 13:24:07 +0100

    updating projects in solution, starting creating dll for unity

---
+++ Engine.dll.debug/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.dll.debug
+{
+    public class Class1
+    {
+    }
+}
