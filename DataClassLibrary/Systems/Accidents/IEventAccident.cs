39e108a360e78a13f442a4720de33cee1a2274fe
Pawel <pplowucha@gmail.com>
2016-12-13 18:36:11 +0100

    Implementacja i przeciążanie interfaców dla IEventAccident

--- DataClassLibrary/Systems/Accidents/IEventAccident.cs
+++ DataClassLibrary/Systems/Accidents/IEventAccident.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MainEngineAlfa.Objects;
 namespace DataClassLibrary
 {
     public interface IEventAccident
     {
-        // nie ma dostępu do klasy Card (pls help)
-        //int Fire(Card cardToIgnite);
-        //muahahahaha
-        int Flood();
-        int EarthQuake();
-        int Hurricane();
-        int Storm();
-        int Drought();
+        
+        void Fire(ICard cardToIgnite);
+        void Fire(ICard card1, ICard card2);
+        void Fire(List<ICard> cardsPack);
+
+
+        void Flood(ICard cardToSink);
+        void Flood(ICard card1, ICard card2);
+        void Flood(List<ICard> cardsPack);
+
+
+        void EarthQuake(ICard cardToShake);
+        void EarthQuake(ICard card1, ICard card2);
+        void EarthQuake(List<ICard> cardsPack);
+
+        void Hurricane(ICard cardToBlow);
+        void Hurricane(ICard card1, ICard card2);
+        void Hurricane(List<ICard> cardsPack);
+
+        void Storm(ICard cardToStorm);
+        void Storm(ICard card1, ICard card2);
+        void Storm(List<ICard> cardsPack);
+
+        void Drought(ICard cardToShake);
+        void Drought(ICard card1, ICard card2);
+        void Drought(List<ICard> cardsPack);
+
+        int CheckAccidentPercentage(ICard cardToCheck);
+        ICard CheckAccidentPercentage(ICard card1, ICard card2);
+        ICard CheckAccidentPercentage(List<ICard> cardsPack);
+
 
     }
 }
